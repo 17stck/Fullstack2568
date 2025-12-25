0d43ea0d813e420ddbe7ada030bf1e96d0077bdf
17stck <komchantipsod1@gmail.com>
2025-12-25 13:38:48 +0700

    25122568

---
+++ Lecture-03/array_loop.js
@@ -0,0 +1,18 @@
+const names = ["Justin", "Sarah", "Christoher"];
+
+
+for (let i = 0; i < names.length; i++) {
+  console.log(names[i]);
+}
+
+names.forEach(name => {
+    console.log(name);
+});
+
+function myFunction(name) {
+    console.log(name);
+}
+
+for (let name of names) {
+    console.log(name);
+}
